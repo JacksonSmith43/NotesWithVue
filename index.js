13ca2bb4cf220d0a3cf1baae29935ebbe9d5f37b
JacksonSmith43 <jessica444@gmx.at>
2023-10-14 11:19:23 +0200

    Improve User Experience, Extend Model.

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            notes: ["Notes 1", "Notes 2", "Notes 3"],
+            notes: [
+                { id: "6769c9ba771", title: "Title 1", text: "ToDo 1" },
+                { id: "dc19d1538f", title: "Title 2", text: "ToDo 2" },
+                { id: "fd8c75b4fb", title: "Title 3", text: "ToDo 3" },
+            ],
+            title: null,
             text: null,
         };
     },
     methods: {
         add() {
-            if (this.text) {
-                this.notes.push(this.text);
-                this.text = "";
+            if (this.title) {
+                this.notes.push(this.title);
+                this.title = "";
                 this.$refs.input.focus();
             }
         },
+        handleClickLIItem(position) {
+            this.notes.splice(position, 1);
+        }
     },
 }).mount("#app");
